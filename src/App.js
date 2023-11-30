4cc410b3e197903526f140cfe069eddd56dd6d9a
meganprice27 <149601780+meganprice27@users.noreply.github.com>
2023-11-30 15:34:10 +0000

    Added GitHub and portfolio URLs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">Weather App
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://marvelous-queijadas-2d1dd5.netlify.app/"
+          target="_blank"
+        >
+          Megan Price
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/meganprice27/react-weather-app"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
-
-
