10a2ad846a3180ce4fa20db1709b06473cdaefc4
meganprice27 <149601780+meganprice27@users.noreply.github.com>
2023-11-30 16:06:49 +0000

    Added Bootstrap and ran audit fix

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://marvelous-queijadas-2d1dd5.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Megan Price
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/meganprice27/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://marvelous-queijadas-2d1dd5.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Megan Price
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/meganprice27/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
